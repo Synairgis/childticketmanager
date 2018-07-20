17baa30c9895dfbc92e4e629209ddc698b4a6d81
Tobbi Filteau <t.filteau@synairgis.com>
2018-07-20 08:56:17 -0400

    Mise à jour du numéro de version

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
   
-define('PLUGIN_CHILDTICKETMANAGER_VERSION', '1.2.3');
+define('PLUGIN_CHILDTICKETMANAGER_VERSION', '1.2.4');
 
 /**
  * Init hooks of the plugin.
